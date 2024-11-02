a64ca97cf97a7b72fadb91f87b8392d5a9cc1d04
Akshu2008wd <akshaykumar123webdeveloper@gmail.com>
2024-11-02 19:52:58 +0530

    operators

---
+++ 2-11-24/operators.py
@@ -0,0 +1,20 @@
+a=345
+b=234
+
+print("Addition" , a+b)
+print("sub-" , a-b)
+print("mul-" , a*b)
+print("div-" , a/b)
+print("f div-" , a//b)
+print("expo-" , a**5)
+
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+expretion = a*b+a-b+(a*b)/a
+
+print(expretion)
