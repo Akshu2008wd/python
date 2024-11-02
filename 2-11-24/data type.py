c1ae08e7c6b3d84a0133d2501c234566d9153ec9
Akshu2008wd <akshaykumar123webdeveloper@gmail.com>
2024-11-02 19:34:48 +0530

    data type 2-11-24

---
+++ 2-11-24/data type.py
@@ -0,0 +1,16 @@
+name = "Akshay";
+age = 16;
+id = "web dev";
+height = 5.8;
+
+
+print(type(name))
+print(type(age))
+print(type(id))
+print(type(height))
+
+#after type casting
+
+height = int(height)
+print(height)
+print(type(height))
