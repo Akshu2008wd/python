c0763f8a6b2d29d82ce0cde85e2594f3955205b4
Akshu2008wd <akshaykumar123webdeveloper@gmail.com>
2024-11-03 18:27:01 +0530

    afcp

---
+++ AfterClassProject.py/textswapping.py
@@ -0,0 +1,25 @@
+a=1
+b=2
+c=3
+
+print(f"The value of is {a} and b is {b} and c is {c} ")
+
+tem=a
+a=b
+b=tem
+
+print(f"The value of is {a} and b is {b} and c is {c}")
+
+
+tem1=b
+b=c
+c=tem1
+
+print(f"The value of is {a} and b is {b} and c is {c}")
+
+tem2=c
+c=a
+a=tem2
+
+print(f"The value of is {a} and b is {b} and c is {c}")
+
