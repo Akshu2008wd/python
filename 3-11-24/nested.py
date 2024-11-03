bb756f7153e63deab6491a89bff4c5a3b30573a8
Akshu2008wd <akshaykumar123webdeveloper@gmail.com>
2024-11-03 19:52:40 +0530

    nested

---
+++ 3-11-24/nested.py
@@ -0,0 +1,11 @@
+num=int(input("enter tne number"))
+
+if num>=50:
+    print("num is greater than 50")
+    if num%2==0:
+        print("num is even") 
+    else:
+        print("num is odd")
+else:
+    print("num is less than 50")
+    
