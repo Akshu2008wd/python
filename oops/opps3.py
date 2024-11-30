479ef3e4c29e008e00b7853ba8991a29b7ed6900
Akshu2008wd <akshaykumar123webdeveloper@gmail.com>
2024-11-30 19:56:50 +0530

    opps

---
+++ oops/opps3.py
@@ -0,0 +1,19 @@
+class parrot:
+    species="bird"
+    
+    def __init__ (self,name,age):
+        self.name=name
+        self.age=age
+        
+peacock=parrot("peacock",10)
+sparrow=parrot("sprrow",20)
+
+print(peacock.name)
+print(peacock.age)
+print(peacock.species)
+
+print(sparrow.name)
+print(sparrow.age)
+print(sparrow.species)
+      
+        
