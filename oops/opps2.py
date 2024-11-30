3881f4b505ab56af75d0a20dcd0162baf484f73f
Akshu2008wd <akshaykumar123webdeveloper@gmail.com>
2024-11-30 19:43:08 +0530

    opps

---
+++ oops/opps2.py
@@ -0,0 +1,12 @@
+class languages:
+    list = ["Java","Python","PHP","C++"]
+    
+    def lan (self):
+        print("I am learning...")
+        
+    def lan2 (self):
+        print("I love", self.list[0])
+        
+x=languages()
+x.lan()
+x.lan2()
