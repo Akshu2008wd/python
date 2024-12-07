ae9560f64a1d5609dae4506cd884c9bac631d1be
Akshu2008wd <akshaykumar123webdeveloper@gmail.com>
2024-12-07 20:00:24 +0530

    opp

---
+++ opps2.py/li,man.py
@@ -0,0 +1,36 @@
+
+class Library:
+    def __init__(self, listofBooks):
+        self.books = listofBooks
+    def displayAvailableBooks(self):
+        print(f"\n{len(self.books)} AVAILABLE BOOKS ARE: ")
+        for book in self.books:
+            print(book)
+        print("\n")
+        
+        
+    def borrowBook(self,bookname):
+        if bookname not in self.books:
+            print(
+                f"{bookname} BOOK IS NOT AVAILABLE EITHER TAKEN BY SOMEONE ELSE, WAIT UNTIL HE RETURNED.\n")
+        else:
+            print("BOOK ISSUED : THANK YOU KEEP IT WITH CARE AND RETURN ON TIME.\n")
+            self.books.remove(bookname)
+            
+            
+            
+    def returnBook(self, bookname):
+        print("BOOK RETURNED : THANK YOU! \n")
+        self.books.append(bookname)
+        
+        
+    def donateBook(self, bookname):
+        print("BOOK DONATED : THANK YOU VERY MUCH, HAVE A GREAT DAY AHEAD.\n")
+        self.books.append(bookname)
+    
+Delhilibrary = Library( ["vistas", "invention", "rich&poor", "indian", "macroeconomics", "microeconomics"])
+print(" WELCOME TO THE DELHI LIBRARY\n")
+
+
+
+
